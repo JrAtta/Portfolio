f421c2b0bdf620da5c5c82a457995c5ddc07d7f4
heka <Aee23530@gmail.com>
2025-08-23 09:53:13 +0300

    add loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, viewChild, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
   btnVisible :boolean = false;
+  loaderVisible :boolean = true;
+  // @ViewChild("loader") loader! : ElementRef;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loaderVisible = false;
+  },2000);
+}
 
   @HostListener('window : scroll',[])
   onWindowScroll():void{
